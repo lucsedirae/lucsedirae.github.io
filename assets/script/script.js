1d5866e2f4ee960185513ce105e33585ff712da9
LucSedirae <jondeavers@gmail.com>
2020-09-22 01:45:53 -0400

    Menu functionality fully working. Ready for content

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,6 @@
 //Global variables
 var menuList = ["About", "Portfolio", "Contact"]
 
-
 //Script begin
 Init();
 
@@ -14,13 +13,29 @@ function Init() {
     listeners();
     }
 
-
-
-
-
 function listeners() {
-    
-    
-    };    
 
+    $("#navBtn0").click(function(){
+        $("#about-jumbo").hide().css("visibility", "visible").fadeIn(800);
+        $("#portfolio-jumbo").css("visibility", "hidden").fadeOut(800);
+        $("#contact-jumbo").css("visibility", "hidden").fadeOut(800);
+      });
 
+      $("#navBtn1").click(function(){
+        $("#portfolio-jumbo").hide().css("visibility", "visible").fadeIn(800);
+        $("#about-jumbo").css("visibility", "hidden");
+        $("#contact-jumbo").css("visibility", "hidden");
+      });
+
+      $("#navBtn2").click(function(){
+        $("#contact-jumbo").hide().css("visibility", "visible").fadeIn(800);
+        $("#portfolio-jumbo").css("visibility", "hidden");
+        $("#about-jumbo").css("visibility", "hidden");
+      });
+
+      $("#my-name").click(function(){
+        $("#contact-jumbo").fadeOut(500);
+        $("#portfolio-jumbo").fadeOut(500);
+        $("#about-jumbo").fadeOut(500);
+      });
+};    
