7e7b338c8f8ae1aa0055ea30fc791426516f40a6
Jon Deavers <jondeavers@gmail.com>
2021-02-10 13:08:03 -0500

    Updated Q form

--- src/components/pages/Questionnaire.js
+++ src/components/pages/Questionnaire.js
@@ -4,13 +4,32 @@ import React from "react";
 //* MUI comps, hooks, icons
 import Container from "@material-ui/core/Container"
 
+// const encode = (data) => {
+//     return Object.keys(data)
+//         .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+//         .join("&");
+//   }
 
+//* Exported component
 const Questionnaire = () => {
-    document.getElementById("root")
+    // document.getElementById("root")
     
+  //  const handleSubmit = e => {
+  //       fetch("/", {
+  //         method: "POST",
+  //         headers: { "Content-Type": "application/x-www-form-urlencoded" },
+  //         body: encode({ "form-name": "contact", ...this.state })
+  //       })
+  //         .then(() => alert("Success!"))
+  //         .catch(error => alert(error));
+    
+  //       e.preventDefault();
+  //     };
+
   return (
     <Container style={{padding: "2rem"}}>
-      <form name="questionnaire">
+      {/* <form onSubmit={handleSubmit} name="questionnaire"> */}
+      <form name="questionnaire" method="post">
         <input type="hidden" name="form-name" value="questionnaire" />
         <p>
           <label>
